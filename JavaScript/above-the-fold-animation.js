7d93241dcac7f2b06e1e484bd32a0a08d2dec38d
itokonnya <124262891+itokonnya@users.noreply.github.com>
2023-02-22 23:11:32 +0900

    Add files via upload

--- JavaScript/above-the-fold-animation.js
+++ JavaScript/above-the-fold-animation.js
@@ -1,10 +1,9 @@
 class AboveTheFoldAnimation {
     constructor() {
         this.body = document.getElementById('body');
-        this.loader = document.getElementById('loader');
-        this.header = document.getElementById('header');
-        this.codeContainer = document.getElementById('code-container');
-        this.repaintButton = document.getElementById('repaint-button');
+        this.loader = document.getElementById( 'loader');
+        this.mainTitle = document.getElementById('main-title');
+        this.subTitle = document.getElementById('sub-title');
         this.count = 0;
         this.countMax = 100;
         this.delta = 10;
@@ -35,13 +34,13 @@ class AboveTheFoldAnimation {
                 } else {
                     this.phase = 2;
                     this.count = 0;
-                    this.loader.style.display = 'none'; // 'block' to 'none'
+                    this.loader.style.display = 'none'; //'block' to 'none'
                 }
                 window.requestAnimationFrame(this.execution);
             } else if (this.phase === 2) {
                 if (this.count < this.countMax) {
-                    this.count = this.count + this.delta;
-                    this.codeContainer.style.opacity = (this.count / this.countMax) * 0.5; //0 to 0.5
+                    this.count = this.count + (this.delta * 0.5);
+                    this.mainTitle.style.opacity = (this.count / this.countMax) * 0.5; //0 to 0.5
                 } else {
                     this.phase = 3;
                     this.count = 0;
@@ -49,9 +48,9 @@ class AboveTheFoldAnimation {
                 window.requestAnimationFrame(this.execution);
             } else if (this.phase === 3) {
                 if (this.count < this.countMax) {
-                    this.count = this.count + this.delta;
-                    this.codeContainer.style.opacity = (this.count / this.countMax) * 0.5 + 0.5; //0.5 to 1
-                    this.repaintButton.style.opacity = (this.count / this.countMax) * 0.5; //0 to 0.5
+                    this.count = this.count + (this.delta * 0.5);
+                    this.mainTitle.style.opacity = (this.count / this.countMax) * 0.5 + 0.5; //0.5 to 1
+                    this.subTitle.style.opacity = (this.count / this.countMax) * 0.5; //0 to 0.5
                 } else {
                     this.phase = 4;
                     this.count = 0;
@@ -59,18 +58,8 @@ class AboveTheFoldAnimation {
                 window.requestAnimationFrame(this.execution);
             } else if (this.phase === 4) {
                 if (this.count < this.countMax) {
-                    this.count = this.count + this.delta;
-                    this.repaintButton.style.opacity = (this.count / this.countMax) * 0.5 + 0.5; //0.5 to 1
-                    this.header.style.opacity = (this.count / this.countMax) * 0.5; //0 to 0.5
-                } else {
-                    this.phase = 5;
-                    this.count = 0;
-                }
-                window.requestAnimationFrame(this.execution);
-            } else if (this.phase === 5) {
-                if (this.count < this.countMax) {
-                    this.count = this.count + this.delta;
-                    this.header.style.opacity = (this.count / this.countMax) * 0.5 + 0.5; //0.5 to 1
+                    this.count = this.count + (this.delta * 0.5);
+                    this.subTitle.style.opacity = (this.count / this.countMax) * 0.5 + 0.5; //0.5 to 1
                 } else {
                     this.phase = null;
                 }
